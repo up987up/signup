678a36e8e7635e796beb99369724a998e8240e88
up987up <36799549+up987up@users.noreply.github.com>
2024-06-24 12:32:20 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.DATABASE_URL ? true : false,
 });
 
 module.exports = pool;
